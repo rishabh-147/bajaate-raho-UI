6e4b0ad8d057d86640d62f65983824eded36f47a
rishabh-147 <onlinestuff.rishabh@gmail.com>
2026-07-12 00:15:30 +0530

    feat: update API base URL configuration for development and production environments

--- src/api/musicApi.js
+++ src/api/musicApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.39:8080/v1";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
